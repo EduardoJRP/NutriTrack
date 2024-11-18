59c97d2dbb4d02c8999da8c51bc091b0ad41970c
Eduardo Rojas <edu.rojas.paez@gmail.com>
2024-11-17 18:29:43 -0600

    feat: added new component

---
+++ app/components/ItemCard.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default function ItemCard() {
+  return (
+    <div className="">
+      <div>
+        <div className=''>Name</div>
+        <div>Grams</div>
+        <div>Calories</div>
+        <div>Carbohidrates</div>
+        <div>Proteins</div>
+        <div>Fats</div>
+      </div>
+    </div>
+  );
+}
