d4d71ffef1a7b3d5639bdbd3c02d1d5a34415636
Eduardo Rojas <edu.rojas.paez@gmail.com>
2024-12-01 11:23:07 -0600

    fix/feat: finished list, added delete button

--- app/components/ItemCard.tsx
+++ app/components/ItemCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface ItemCardProps {
   name: string;
-  grams: number;
+  quantity: number;
   calories: number;
   carbohydrates: number;
   proteins: number;
@@ -11,22 +11,40 @@ interface ItemCardProps {
 
 export default function ItemCard({
   name,
-  grams,
+  quantity,
   calories,
   carbohydrates,
   proteins,
   fats,
 }: ItemCardProps) {
   return (
-    <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-md">
-      <div className="grid grid-cols-6 gap-4 text-center font-semibold text-gray-700">
-        <div className="col-span-2">Name: {name}</div>
-        <div>Grams: {grams}</div>
-        <div>Calories: {calories}</div>
-        <div>Carbohydrates: {carbohydrates}</div>
-        <div>Proteins: {proteins}</div>
-        <div>Fats: {fats}</div>
+    <div className="flex flex-row">
+      <div className="w-full rounded-lg border border-gray-200 bg-white p-4 shadow-md">
+        <div className="grid grid-cols-6 gap-4 text-center font-semibold text-gray-700">
+          <div>Name: {name}</div>
+          <div>Quantity: {quantity}g</div>
+          <div>Calories: {calories}</div>
+          <div>Carbohydrates: {carbohydrates}</div>
+          <div>Proteins: {proteins}</div>
+          <div>Fats: {fats}</div>
+        </div>
       </div>
+      <button className="w-1/8 bg-orange-500 text-white font-semibold rounded-lg py-2 px-4 m-4 hover:bg-orange-600 transition-all">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
--- app/components/ItemList.tsx
+++ app/components/ItemList.tsx
@@ -3,31 +3,36 @@ import ItemCard from './ItemCard';
 
 interface ItemData {
   name: string;
-  grams: number;
+  quantity: number;
   calories: number;
   carbohydrates: number;
   fats: number;
   proteins: number;
 }
 
-export default function AddItemForm() {
+export default function ItemList() {
   const [items, setItems] = useState<ItemData[]>([]);
 
   useEffect(() => {
     async function fetchItems() {
       try {
-        const response = await fetch('api/submitForm');
+        const response = await fetch('/api/submitForm');
         const data = await response.json();
-        data.success
-          ? setItems(data.data)
-          : console.error(`Error fetching items: ${data.error}`);
+        if (data.success) {
+          setItems(data.data);
+        } else {
+          console.error(`Error fetching items: ${data.error}`);
+        }
       } catch (error) {
         console.error(`Network error: ${error}`);
       }
     }
-
     fetchItems();
   }, []);
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
 
   return (
     <div className="space-y-4">
@@ -35,7 +40,7 @@ export default function AddItemForm() {
         <ItemCard
           key={index}
           name={item.name}
-          grams={item.grams}
+          quantity={item.quantity}
           calories={item.calories}
           carbohydrates={item.carbohydrates}
           proteins={item.proteins}
