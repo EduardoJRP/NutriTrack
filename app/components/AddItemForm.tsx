5a58edaa1bebaa82b2a2df36aa15d859195faf91
Eduardo Rojas <edu.rojas.paez@gmail.com>
2024-12-29 19:57:31 -0600

    fix: changed AddItemForm size for sm screens

--- app/components/AddItemForm.tsx
+++ app/components/AddItemForm.tsx
@@ -61,79 +61,79 @@ export default function AddItemForm() {
   };
 
   return (
-    <div className="bg-orange-300 rounded-xl p-5 max-w-lg w-full">
-      <h1 className="text-xl font-bold">Add the info of the item</h1>
+    <div className="bg-orange-300 rounded-xl p-5 sm:p-8 max-w-lg sm:max-w-md w-full sm:h-auto sm:m-10">
+      <h1 className="text-xl sm:text-lg font-bold">Add the info of the item</h1>
       <div className="flex flex-col w-full">
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Name of the food</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Name of the food</span>
           <input
             name="name"
             type="text"
             placeholder="e.g. Egg"
             value={itemData.name}
             onChange={handleChange}
-            className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Grams</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Grams</span>
           <input
             name="quantity"
             type="number"
             placeholder="100"
             value={itemData.quantity}
             onChange={handleChange}
-            className="no-arrows border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="no-arrows border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Calories</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Calories</span>
           <input
             name="calories"
             type="number"
             placeholder="200"
             value={itemData.calories}
             onChange={handleChange}
-            className="no-arrows border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="no-arrows border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Carbohydrates</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Carbohydrates</span>
           <input
             name="carbohydrates"
             type="number"
             placeholder="18.1"
             value={itemData.carbohydrates}
             onChange={handleChange}
-            className="no-arrows border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="no-arrows border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Fats</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Fats</span>
           <input
             name="fats"
             type="number"
             placeholder="18.1"
             value={itemData.fats}
             onChange={handleChange}
-            className="no-arrows border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="no-arrows border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
-        <label className="flex flex-col w-full p-3">
-          <span className="font-semibold text-lg">Proteins</span>
+        <label className="flex flex-col w-full p-3 sm:px-0">
+          <span className="font-semibold text-lg sm:text-sm">Proteins</span>
           <input
             name="proteins"
             type="number"
             placeholder="18.1"
             value={itemData.proteins}
             onChange={handleChange}
-            className="no-arrows border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
+            className="no-arrows border border-gray-300 rounded-lg p-3 sm:p-1 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </label>
         <button
           type="submit"
           onClick={handleSubmit}
-          className="w-full bg-orange-500 text-white font-semibold rounded-lg py-2 px-4 mt-4 hover:bg-orange-600 transition-all"
+          className="w-48 self-center bg-orange-500 text-white font-semibold rounded-lg py-2 px-4 mt-4 hover:bg-orange-600 transition-all"
         >
           Submit!
         </button>
