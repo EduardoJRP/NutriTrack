60cf75029c0b6da96e180c7f88ee119e6629a6bb
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-02-25 20:39:57 -0600

    update: added scratch for the UserInfo component

--- app/components/Main/UserInfo.tsx
+++ app/components/Main/UserInfo.tsx
@@ -7,8 +7,13 @@ interface UserWelcomeCardProps {
 const UserInfo: React.FC<UserWelcomeCardProps> = ({ className }) => {
   return (
     <div className={`m-6 p-6 rounded-xl shadow-lg ${className}`}>
-      <h1>Here is your info User:</h1>
-      <h3></h3>
+      <h2 className='text-5xl'>Here is your info User:</h2>
+      <ul className='list-disc list-inside m-4'>
+        <li className='text-xl m-2'>Weight: 100g</li>
+        <li className='text-xl m-2'>Height: 180m</li>
+        <li className='text-xl m-2'>Fat Percentage: 12%</li>
+        <li className='text-xl m-2'>Age: 25</li>
+      </ul>
     </div>
   );
 };
