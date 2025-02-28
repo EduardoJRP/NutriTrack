28718666288bd2402d39f8bcb0aba29343ba492f
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-02-27 20:39:54 -0600

    update: added new page for users information

--- app/itemList/page.tsx
+++ app/itemList/page.tsx
@@ -6,7 +6,7 @@ import ItemList from '../components/ItemList/ItemList';
 export default function AddItem() {
   return (
     <div className="min-h-screen flex flex-col">
-      <NavBar></NavBar>
+      <NavBar/>
       <div className="flex flex-grow items-center justify-center w-full">
         <ItemList></ItemList>
       </div>
---
+++ app/userInfo/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NavBar from '../components/Common/NavBar';
+
+export default function AddItem() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+    </div>
+  );
+}
