fa23a18e3077e2654537b6d9a62ed1909ae18cd7
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-09-13 18:28:08 -0500

    update: added a logo

--- app/components/Common/Navbar.tsx
+++ app/components/Common/Navbar.tsx
@@ -1,20 +1,32 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold text-green-600">NutriTrack</h1>
-      <ul className="flex gap-6 text-gray-600">
-        <li className="hover:text-green-600 cursor-pointer">
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li className="hover:text-green-600 cursor-pointer">
-          <Link href="/recipes">Recipes</Link>
-        </li>
-        <li className="hover:text-green-600 cursor-pointer"><Link href="/intakeLog">Intake Log</Link></li>
-        <li className="hover:text-green-600 cursor-pointer"><Link href="/profile">Profile</Link></li>
-        <li className="hover:text-green-600 cursor-pointer"><Link href="/help">Help</Link></li>
-      </ul>
+      <Link className="flex items-center gap-2" href="/">
+        <Image src="/logo.png" alt="NutriTrack Logo" width={40} height={40} />
+        <h1 className="text-xl font-bold text-green-600">NutriTrack</h1>
+      </Link>
+      <div>
+        <ul className="flex gap-6 text-gray-600">
+          <li className="hover:text-green-600 cursor-pointer">
+            <Link href="/">Dashboard</Link>
+          </li>
+          <li className="hover:text-green-600 cursor-pointer">
+            <Link href="/recipes">Recipes</Link>
+          </li>
+          <li className="hover:text-green-600 cursor-pointer">
+            <Link href="/intakeLog">Intake Log</Link>
+          </li>
+          <li className="hover:text-green-600 cursor-pointer">
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li className="hover:text-green-600 cursor-pointer">
+            <Link href="/help">Help</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
